2dacf371c62a5483dbe4b46753af2a093d3aecc5
Yan Beleli <yanbelelli@gmail.com>
2026-05-02 19:45:29 -0300

    fix: github pages deployment config

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx'
 import AuthPage from './pages/AuthPage.jsx'
 import AdminPage from './pages/AdminPage.jsx'
 
 export default function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/auth" element={<AuthPage />} />
                 <Route path="/admin" element={<AdminPage />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
